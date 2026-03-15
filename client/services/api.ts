6d9e0f11b72eba5701ec029ae5a51dda01fb1b9f
Velmurugan <velmuruganrbusiness@gmail.com>
2026-03-15 10:00:13 +0530

    ap url change

--- client/services/api.ts
+++ client/services/api.ts
@@ -8,7 +8,7 @@ export interface CommentResponse {
 }
 
 // const API_URL = 'http://localhost:5000/api';
-const API_URL = 'http://127.0.0.1:5000/api';
+const API_URL = 'http://127.0.0.1/api';
 export const api = {
   // Check if server is reachable
   checkHealth: async (): Promise<boolean> => {
